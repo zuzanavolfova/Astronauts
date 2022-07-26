9661fa493890a9dd4e803ec4c9e9cccba8345515
Zuzana Volfova <zuzka.volfova@gmail.com>
2022-07-26 14:29:15 +0200

    add card UI funcionality

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+let A = document.querySelector('.list');
+
 function addAstronaut(){
     addListItem();
 }
@@ -26,4 +28,46 @@ function addListItem (){
     
     list.push(item);
     // console.log(list)
+
+    
+    let listItem = document.createElement('div');
+    listItem.className = 'list__item';
+    A.appendChild(listItem);
+
+    let listName = document.createElement('div');
+    listItem.className = 'list__name';
+    listItem.appendChild(listName);
+    
+    let listNameAstronaut = document.createElement('span');
+    listNameAstronaut.className = 'list__content bold name';
+    listNameAstronaut.textContent = jmeno;
+    listName.appendChild(listNameAstronaut);
+}
+
+function displayItem ( jmeno, prijmeni, datumNarozeni, superschopnost){
+    let listClass = document.querySelector('#list');
+    
+    let listItem = document.createElement('div');
+    listItem.className = 'list__item';
+    listClass.appendChild(listItem);
+
+    let listName = document.createElement('div');
+    listItem.className = 'list__name';
+    listItem.appendChild(listName);
+    
+    let listNameAstronaut = document.createElement('span');
+    listNameAstronaut.className = 'list__content bold name';
+    listNameAstronaut.textContent = jmeno;
+    listName.appendChild(listNameAstronaut);
+    console.log (jmeno)
+   
+}
+
+function createDiv(className){
+    let div = document.createElement('div');
+    div.className = className;
+}
+function createSpan(className){
+    let span = document.createElement('span');
+    span.className = className;
 }
